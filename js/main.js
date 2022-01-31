4734575e3e99d19d4c100704b7b6052a5dea350f
MD.JIHAD HOSSAIN <43439618+mdjihad560@users.noreply.github.com>
2022-02-01 00:48:42 +0600

    code clone

---
+++ js/main.js
@@ -0,0 +1,45 @@
+(function($) {
+"use strict";
+
+/*------------------------------------------------------------------
+[Table of contents]
+
+THEPAW TESTIMONIAL SLIDER JS
+THEPAW SWIPER SLIDER JS
+
+-------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------
+CUSTOM PRE DEFINE FUNCTION
+------------------------------------------------------------*/
+/* is_exist() */
+jQuery.fn.is_exist = function(){
+  return this.length;
+}
+
+
+$(function(){
+
+
+
+});/*End document ready*/
+
+
+$(window).on("resize", function(){
+
+
+}); // end window resize
+
+
+$(window).on("load" ,function(){
+
+}); // End window LODE
+
+
+})(jQuery);
+
+
+
+
+
+
